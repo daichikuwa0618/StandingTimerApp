b0c9747a500b8ba98edb9394ed247c5cb476ef39
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-05-24 14:33:36 +0900

    add: R.swift

--- StandingTimerApp/Pods/Target Support Files/Pods-StandingTimerApp/Pods-StandingTimerApp-frameworks.sh
+++ StandingTimerApp/Pods/Target Support Files/Pods-StandingTimerApp/Pods-StandingTimerApp-frameworks.sh
@@ -198,9 +198,11 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LGButton/LGButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/LGButton/LGButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
