d0ff01201f1fadfe50d98dd221db9d5cb3850df6
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-05-24 13:11:20 +0900

    add: initialize project

---
+++ StandingTimerApp/StandingTimerApp/SettingTimerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingTimerViewController.swift
+//  StandingTimerApp
+//
+//  Created by Daichi Hayashi on 2020/05/24.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class SettingTimerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StandingTimerApp/StandingTimerAppTests/StandingTimerAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  StandingTimerAppTests.swift
+//  StandingTimerAppTests
+//
+//  Created by Daichi Hayashi on 2020/05/24.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import XCTest
+@testable import StandingTimerApp
+
+class StandingTimerAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
