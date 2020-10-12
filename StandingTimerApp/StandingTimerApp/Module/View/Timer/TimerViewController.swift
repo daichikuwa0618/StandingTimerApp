43613a680cf5fab01929c0434025e9e8ca25f5e0
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-10-12 20:14:42 +0900

    add: TimerVC, RxSwift

---
+++ StandingTimerApp/StandingTimerApp/Module/View/Timer/TimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimerViewController.swift
+//  StandingTimerApp
+//
+//  Created by 林 大地 on 2020/10/12.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StandingTimerApp/StandingTimerApp/Module/View/Timer/TimerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TimerViewModel.swift
+//  StandingTimerApp
+//
+//  Created by 林 大地 on 2020/10/12.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import Foundation
